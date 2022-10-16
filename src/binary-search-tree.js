2a66225e75e9b15e3e9a1b9750fec26a767b2916
Peter Terentev <petrfalkovsky@yandex.ru>
2022-10-16 23:05:16 +0300

    max min в процессе, пока тесты чота не проходят...

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -7,35 +7,63 @@ class BinarySearchTree {
     this.root = null; // присваиваем изначальный рут = нал
   }
 
-// пробуем реализовать медот добавления
-  add(data) { 
-    const createNode = new Node(data); 
-// создаем инстанс для создания ноды, если ее не было раньше
-    if(!this.root){
+
+  //? .add() пробуем реализовать медот добавления
+  add(data) {
+    const createNode = new Node(data);
+    // создаем инстанс для создания ноды, если ее не было раньше
+    if (!this.root) {
       this.root = createNode;
       return;
     }
-// нам понадобиться изменияемая текущая нода, которую мы будем сортировать налево и направо
+    // нам понадобиться изменияемая текущая нода, которую мы будем сортировать налево и направо
     let currentNode = this.root;
 
-    while(currentNode){
+    while (currentNode) {
       if (createNode.data < currentNode.data) {
-          if (!currentNode.left) {
-            currentNode.left = createNode;
-            return;
-          }
-          currentNode = createNode.left;
-      }
-      else { 
-        if (!currentNode.right) {
+        if (!currentNode.left) {
+          currentNode.left = createNode;
+          return;
+        }
+        currentNode = createNode.left;
+      } else if (!currentNode.right) {
         currentNode.right = createNode;
         return;
       }
       currentNode = createNode.right;
+    }
+  }
 
+  //? .find() пробуем найти значение в бинарном дереве  
+  find(data) {
+    return findNode(this.rootData, data);
+
+    function findNode(node, data) {
+      if (!node) {
+        return null
+      } else if (node.data === data) {
+        return node;
+      } else if (node.data > data) {
+        return searchWithin(node.left, value);
+      } else {
+        return searchWithin(node.right, value);
       }
     }
   }
+
+
+   max(Node, node) {
+    let maxValue = node.data;
+    if(node.left != null) {
+        maxValue = Math.maxValue(maxValue, max(node.left));
+    }
+    if(node.right != null) {
+        maxValue = Math.maxValue(maxValue, max(node.right));
+    }
+    return maxValue;
+}
+
+
 
 }
 
@@ -43,8 +71,12 @@ module.exports = {
   BinarySearchTree
 };
 
-// todo: можно протестировать добавление значений в бинарное дерево
-/* 
+
+
+
+
+// todo: .add() - можно протестировать добавление значений в бинарное дерево
+
 // начало .add()
 const createBinaryTree = new BinarySearchTree();
 createBinaryTree.add(30);
@@ -57,11 +89,11 @@ createBinaryTree.add(18);
 createBinaryTree.add(4);
 createBinaryTree.add(11);
 createBinaryTree.add(16);
+createBinaryTree.find(8);
 
-
-console.log(createBinaryTree);
+// console.log(createBinaryTree.has(15));
 // конец .add()
- */
+
 
 
 //   has(/* data */) {
@@ -69,10 +101,6 @@ console.log(createBinaryTree);
 //     // remove line with error and write your code here
 //   }
 
-//   find(/* data */) {
-//     throw new NotImplementedError('Not implemented');
-//     // remove line with error and write your code here
-//   }
 
 //   remove(/* data */) {
 //     throw new NotImplementedError('Not implemented');
