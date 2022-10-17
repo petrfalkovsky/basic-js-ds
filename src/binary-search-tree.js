bf0a363e7355adb09e37568a62d35be4b2002ee3
Peter Terentev <petrfalkovsky@yandex.ru>
2022-10-17 16:54:39 +0300

    все методы реализованы, тесты проходят, фух

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,19 +1,18 @@
-const { NotImplementedError } = require('../extensions/index');
 const { Node } = require('../extensions/list-tree');
 
 class BinarySearchTree {
   constructor() {
-    this.orginRoot = null; // присваиваем изначальный рут - пустой
+    this.originRoot = null; // присваиваем изначальный рут - пустой
   }
 
   //? реализуем подготовку первого узла
   root() {
-    return this.orginRoot;
+    return this.originRoot;
   }
 
   //? .add() пробуем реализовать медот добавления элемента в массив
   add(data) {
-    this.orginRoot = currentNode(this.orginRoot, data);
+    this.originRoot = currentNode(this.originRoot, data);
 
     function currentNode(node, dataValue) {
       // создаем ноду, если ее не было раньше
@@ -33,7 +32,7 @@ class BinarySearchTree {
 
   //? .has() проверяем есть/нет значения в массиве
   has(data) {
-    return hasVelue(this.orginRoot, data);
+    return hasVelue(this.originRoot, data);
 
     function hasVelue(node, data) {
       if (!node) {
@@ -48,9 +47,9 @@ class BinarySearchTree {
     }
   }
 
-   //? .find() пробуем найти значение в бинарном дереве  
+  //? .find() пробуем найти значение в бинарном дереве  
   find(data) {
-    return findVelue(this.orginRoot, data);
+    return findVelue(this.originRoot, data);
 
     function findVelue(node, data) {
       if (!node) {
@@ -68,21 +67,60 @@ class BinarySearchTree {
   //! скорости мы достигаем за счет того, что в процессе отметаем области, где просто не может находится
   //! искомое значение, таким образом область поиска сокращается в прогрессии
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  //? реализуем удаление ноды со значением
+  remove(data) {
+    this.originRoot = deleteData(this.originRoot, data);
+
+    function deleteData(node, data) {
+      if (!node) {
+        return null;
+      }
+
+      if (data < node.data) {
+        node.left = deleteData(node.left, data);
+        return node;
+      } else if (node.data < data) {
+        node.right = deleteData(node.right, data);
+        return node;
+      } else {
+        if (!node.left && !node.right) {
+          return null;
+        } else if (!node.left) {
+          node = node.right;
+          return node;
+        } else if (!node.right) {
+          node = node.left;
+          return node;
+        }
+
+        let minRight = node.right;
+        while (minRight.left) {
+          minRight = minRight.left;
+        }
+        node.data = minRight.data;
+        node.right = deleteData(node.right, minRight.data);
+        return node;
+      }
+    }
   }
 
+  //? находим минимум
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    var minNode = this.originRoot;
+    while (minNode.left) {
+      minNode = minNode.left;
+    }
+    return minNode.data;
   }
 
+  //? находим максимальное значение
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    var maxNode = this.originRoot;
+    while (maxNode.right) {
+      maxNode = maxNode.right;
+    }
+    return maxNode.data;
   }
-
 
 }
 
@@ -90,9 +128,6 @@ module.exports = {
   BinarySearchTree
 };
 
-// todo: .add() - можно протестировать добавление значений в бинарное дерево
-
-// начало .add()
 // const createBinaryTree = new BinarySearchTree();
 // createBinaryTree.add(30);
 // createBinaryTree.add(3);
@@ -104,5 +139,7 @@ module.exports = {
 // createBinaryTree.add(4);
 // createBinaryTree.add(11);
 // createBinaryTree.add(16);
+// createBinaryTree.has(8);
 // createBinaryTree.find(8);
+// createBinaryTree.remove(8);
 // console.log(createBinaryTree.has(15));
